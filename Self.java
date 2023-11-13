b4630c55e1a0e3ac2242463b486456a901a9e611
adhithya <adhithyachikku10@gmail.com>
2023-11-13 12:10:42 +0530

    Self.java

---
+++ Self.java
@@ -0,0 +1,11 @@
+     class Self {
+
+     public static void main(String[] args) {
+              String myName ="Adhithya";
+             int age = 22 ;
+             System.out.println("myName");
+             System.out.println("age");
+     }
+    
+
+     }
