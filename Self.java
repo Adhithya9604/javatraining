2f3780d3385cae3429d768a1f8b126c5eb3d2cf0
adhithya <adhithyachikku10@gmail.com>
2023-11-15 20:42:41 +0530

    Self.java

--- Self.java
+++ Self.java
@@ -3,8 +3,10 @@
      public static void main(String[] args) {
               String myName ="Adhithya";
              int age = 22 ;
-             System.out.println("myName");
-             System.out.println("age");
+             String location ="Trivandrum";
+             String profession ="Student";  
+             System.out.println(myName);
+             System.out.println(age);
      }
     
 
