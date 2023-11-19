31dea222789f64c41a1aa99fb7a50d62341224e0
adhithya <adhithyachikku10@gmail.com>
2023-11-19 15:33:48 +0530

    conditions.java

---
+++ conditions.java
@@ -0,0 +1,35 @@
+class conditions{
+
+
+    public static void main(String[] args) {
+    int balance =99;
+    if(balance>=100)
+
+    {
+      System.out.println("Your balance is above");
+
+      if (balance>=100 && balance<=200)
+      {
+         System.out.println("Your balance is in between 100 and 200");
+        
+      }
+      
+      else
+      {
+        System.out.println("Your balance is above 200");
+
+      }
+       
+
+    }
+    }
+    
+
+
+
+
+
+
+
+
+}
