4d31e999c8ecd2f1d3060f897e28f8fedb612b01
adhithya <adhithyachikku10@gmail.com>
2023-11-20 10:42:08 +0530

    ForLoopExample.java

---
+++ ForLoopExample.java
@@ -0,0 +1,13 @@
+class ForLoopExample {
+    public static void main(String[] args) {
+
+        int multiplicationFor = 4;
+
+        for (int i = 0; i <= 10; i++) {
+
+            System.out.printf("%d * %d =%d \n", i, multiplicationFor, i * multiplicationFor);
+        }
+
+    }
+
+}
