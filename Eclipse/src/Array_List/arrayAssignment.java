4f74d470e8c5b404d0202f70361f08ff35851152
adhithya <adhithyachikku10@gmail.com>
2024-01-11 20:14:37 +0530

    Array_List

--- Eclipse/src/Array_List/arrayAssignment.java
+++ Eclipse/src/Array_List/arrayAssignment.java
@@ -6,7 +6,7 @@ public class arrayAssignment {
 
 	public static void main(String[] args) {
      ArrayList<String> colours = new ArrayList();
-     // for (String i = 0; i < 6; i++) {
+     
      colours.add("Black");
       colours.add("Green");
       colours.add("Blue");
@@ -15,7 +15,7 @@ public class arrayAssignment {
       colours.add("Orange");
       
        System.out.println(colours);
-      //System.out.println(colours.indexOf("Colours in the ArrayList"+ colours));
+      
        System.out.println("Retrive element : "+ colours.get(0));
        System.out.println("Remove element : " + colours.remove(1) );
        System.out.println("Check element: "+ colours.contains(10));
